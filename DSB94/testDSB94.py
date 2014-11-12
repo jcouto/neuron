6204fc3691de64896b44c7c07a2f12aa3bbdb763
Joao Couto <jpcouto@gmail.com>
2014-11-12 20:01:11 +0100

    DSB template commented out model selection and set default to the PM10 model.

--- DSB94/testDSB94.py
+++ DSB94/testDSB94.py
@@ -1,29 +1,30 @@
 #!/usr/bin/env python
 from neuron import h
-h.load_file('DSB94template')
+h.load_file('DSB94tmplt')
+h.load_file('stdrun.hoc')
 import numpy as np
 import pylab as plt
 
 def main():
     pkj = h.DSB94()
     h.dt = 0.02
-    Vrest = -50
+    Vrest = -74
     h.celsius = 37
     rec={}
     rec['v'] = h.Vector()
     rec['t'] = h.Vector()
     rec['v'].record(pkj.soma(0.5)._ref_v)  
     rec['t'].record(h._ref_t)
-    h.load_file('stdrun.hoc')
-    vclamp = h.IClamp(pkj.soma(0.5))
-    vclamp.amp = 1
-    vclamp.dur = 20+np.random.uniform(40)
-    vclamp.delay = 0
+    iclamp = h.IClamp(pkj.soma(0.5))
+    iclamp.amp = 0.300
+    iclamp.dur = 1000
+    iclamp.delay = 0
     h.finitialize(Vrest)
-    h.tstop = 400
+    h.tstop = 700
     h.run()
     plt.plot(rec['t'],rec['v'])
     plt.show()
-
+    from ipdb import set_trace
+    set_trace()
 if __name__ == '__main__':
     main()
